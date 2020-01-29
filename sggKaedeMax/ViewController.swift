0d99b1ad6e5667928b4c33b0ba352ceb0416ed0a
renueji <rentaroubiz@gmail.com>
2020-01-29 14:26:36 +0900

    コミット1

---
+++ sggKaedeMax/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  sggKaedeMax
+//
+//  Created by Rentaro on 2020/01/29.
+//  Copyright © 2020 Rentaro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("Hello, World!!")
+        
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
